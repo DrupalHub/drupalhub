da8382d3bbcd5d4392165ca36ff4ff807c115a45
Roy Segall <roy.segall@gizra.com>
2014-06-01 23:52:25 +0300

    Change layout of the front page.

--- drupalhub/modules/drupalhub/drupalhub_core/plugins/layouts/drupalhub_homepage/drupalhub-homepage.tpl.php
+++ drupalhub/modules/drupalhub/drupalhub_core/plugins/layouts/drupalhub_homepage/drupalhub-homepage.tpl.php
@@ -1,25 +1,36 @@
 <div class="<?php print $classes ?>" <?php if (!empty($css_id)) { print "id=\"$css_id\""; } ?>>
   <?php if ($content['first']): ?>
-    <div class="row">
+    <div class="first row">
       <?php print $content['first']; ?>
     </div>
   <?php endif ?>
 
   <?php if ($content['first_left'] || $content['fist_right']): ?>
-    <div class="row">
+    <div class="first-left-rigjt row">
       <?php print $content['first_left']; ?>
       <?php print $content['fist_right']; ?>
     </div>
   <?php endif ?>
 
-  <?php if ($content['second']): ?>
-    <div class="row">
-      <?php print $content['second']; ?>
+  <?php if ($content['second-top']): ?>
+    <div class="second-top row">
+      <?php print $content['second-top']; ?>
+    </div>
+  <?php endif ?>
+  <?php if ($content['second-middle']): ?>
+    <div class="second-middle row">
+      <?php print $content['second-middle']; ?>
+    </div>
+  <?php endif ?>
+
+  <?php if ($content['second-last']): ?>
+    <div class="second-last row">
+      <?php print $content['second-last']; ?>
     </div>
   <?php endif ?>
 
   <?php if ($content['second_left'] || $content['second_right']): ?>
-    <div class="row">
+    <div class="second-left-right row">
       <?php print $content['second_left']; ?>
       <?php print $content['second_right']; ?>
     </div>
