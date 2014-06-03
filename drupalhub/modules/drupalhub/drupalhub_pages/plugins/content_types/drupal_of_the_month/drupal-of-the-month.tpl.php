9c91ccc4bc2022a99637bb5e2cda6a294a9f04a4
Roy Segall <roy.segall@gizra.com>
2014-06-03 20:18:31 +0300

    Working on the drupal of the month.

--- drupalhub/modules/drupalhub/drupalhub_pages/plugins/content_types/drupal_of_the_month/drupal-of-the-month.tpl.php
+++ drupalhub/modules/drupalhub/drupalhub_pages/plugins/content_types/drupal_of_the_month/drupal-of-the-month.tpl.php
@@ -1,33 +1,26 @@
-<div class="row blue-bg padding-15">
-  <h2 class="white no-margin no-padding text-center">דרופל החודש</h2>
-</div>
-<div class="row grey-bg month_overlay">
-  <div class="col-xs-12">
-    <div class="text-center large">אתר דרופל ישראל החדש</div>
-    <div class="text-center padding-top-15 padding-bottom-15">
-      <span class="star big"></span>
-      <span class="star big"></span>
-      <span class="star big"></span>
-      <span class="star big"></span>
-      <span class="star big"></span>
-    </div>
-    <div class="relative" style="padding:0 12px">
-      <img src="/drupal-il/theme/Images/month_screen.png" alt="New site screenshot" style="border:1px solid #fff" width="100%">
-    </div>
-    <div class="row text-center padding-top-15 padding-bottom-15">
-      <div class="col-xs-3"></div>
-      <div class="col-xs-6">
-        <div class="row">
-          <div class="col-xs-4"><div class="ai small views text-right">39</div></div>
-          <div class="col-xs-4"><div class="ai small comments text-right">56</div></div>
-          <div class="col-xs-4"><div class="ai small likes text-right">39</div></div>
-        </div>
+<div class="drupal-of-the-month">
+  <div class="row header">
+    <h2 class="white no-margin no-padding text-center"><?php print t('Drupal of the month'); ?></h2>
+  </div>
+  <div class="row grey-bg month_overlay">
+    <div class="col-xs-12">
+      <div class="text-center title large"><?php print $title; ?></div>
+      <div class="relative" style="padding:0 12px">
+        <?php print $image; ?>
       </div>
-      <div class="col-xs-3"></div>
+      <div class="row text-center ratings">
+        <div class="col-xs-3"></div>
+        <div class="col-xs-6">
+          <div class="row">
+            <div class="col-xs-4"><i class="fa fa-heart"></i> 39</div>
+          </div>
+        </div>
+        <div class="col-xs-3"></div>
+      </div>
+      <div class="text-center">
+        <?php print $body; ?>
+      </div>
+      <div class="group"><?php print t('From the portfolio of: '); ?> <?php print $group_link; ?></div>
     </div>
-    <div class="text-center">
-      some text description about the site of the month should go here ... some text description about the site of the month should go here ...  some text description about the site of the month should go here ... some text description about the site of the month should go here ...
-    </div>
-    <div class="padding-top-15 padding-bottom-15">מהפרוטפוליו של: <a>Shneor Web</a></div>
   </div>
 </div>
