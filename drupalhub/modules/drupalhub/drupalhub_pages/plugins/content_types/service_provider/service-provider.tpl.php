8ea119b7834dd737b778c092cd95500c36fd9dad
Roy Segall <roy.segall@gizra.com>
2014-06-07 00:49:32 +0300

    Replace the stub with a real code. Not complete yet.

--- drupalhub/modules/drupalhub/drupalhub_pages/plugins/content_types/service_provider/service-provider.tpl.php
+++ drupalhub/modules/drupalhub/drupalhub_pages/plugins/content_types/service_provider/service-provider.tpl.php
@@ -1,194 +1,23 @@
-<div class="col-xs-12 no-padding">
-  <div class="col-xs-1 relative first">
-    <div class="arrow first_arrow disabled"></div>
-  </div>
-  <div class="col-xs-10 no-padding">
-    <div class="row">
-      <div class="col-xs-2">
-        <div class="service_item">
-          <div class="overflow height_100"><img src="https://lh6.googleusercontent.com/-ehecd-e2r1g/UsyI-rezM1I/AAAAAAAAAE8/Oma14zsJw1w/s0-d/shneor-web-logo-plus.jpg" width="100%" alt="logo"></div>
-          <div class="text">
-            <div class="text-center">shneurweb</div>
-            <div class="text-center">
-              <span class="star"></span>
-              <span class="star"></span>
-              <span class="star"></span>
-              <span class="star"></span>
-              <span class="star"></span>
-            </div>
-          </div>
-        </div>
-      </div>
-      <div class="col-xs-2">
-        <div class="service_item">
-          <div class="overflow height_100"><img src="https://lh6.googleusercontent.com/-ehecd-e2r1g/UsyI-rezM1I/AAAAAAAAAE8/Oma14zsJw1w/s0-d/shneor-web-logo-plus.jpg" width="100%" alt="logo"></div>
-          <div class="text">
-            <div class="text-center">shneurweb</div>
-            <div class="text-center">
-              <span class="star"></span>
-              <span class="star"></span>
-              <span class="star"></span>
-              <span class="star"></span>
-              <span class="star"></span>
-            </div>
-          </div>
-        </div>
-      </div>
-      <div class="col-xs-2">
-        <div class="service_item">
-          <div class="overflow height_100"><img src="https://lh6.googleusercontent.com/-ehecd-e2r1g/UsyI-rezM1I/AAAAAAAAAE8/Oma14zsJw1w/s0-d/shneor-web-logo-plus.jpg" width="100%" alt="logo"></div>
-          <div class="text">
-            <div class="text-center">shneurweb</div>
-            <div class="text-center">
-              <span class="star"></span>
-              <span class="star"></span>
-              <span class="star"></span>
-              <span class="star"></span>
-              <span class="star"></span>
-            </div>
-          </div>
-        </div>
-      </div>
-      <div class="col-xs-2">
-        <div class="service_item">
-          <div class="overflow height_100"><img src="https://lh6.googleusercontent.com/-ehecd-e2r1g/UsyI-rezM1I/AAAAAAAAAE8/Oma14zsJw1w/s0-d/shneor-web-logo-plus.jpg" width="100%" alt="logo"></div>
-          <div class="text">
-            <div class="text-center">shneurweb</div>
-            <div class="text-center">
-              <span class="star"></span>
-              <span class="star"></span>
-              <span class="star"></span>
-              <span class="star"></span>
-              <span class="star"></span>
-            </div>
-          </div>
-        </div>
-      </div>
-      <div class="col-xs-2">
-        <div class="service_item">
-          <div class="overflow height_100"><img src="https://lh6.googleusercontent.com/-ehecd-e2r1g/UsyI-rezM1I/AAAAAAAAAE8/Oma14zsJw1w/s0-d/shneor-web-logo-plus.jpg" width="100%" alt="logo"></div>
-          <div class="text">
-            <div class="text-center">shneurweb</div>
-            <div class="text-center">
-              <span class="star"></span>
-              <span class="star"></span>
-              <span class="star"></span>
-              <span class="star"></span>
-              <span class="star"></span>
-            </div>
-          </div>
-        </div>
-      </div>
-      <div class="col-xs-2">
-        <div class="service_item">
-          <div class="overflow height_100"><img src="https://lh6.googleusercontent.com/-ehecd-e2r1g/UsyI-rezM1I/AAAAAAAAAE8/Oma14zsJw1w/s0-d/shneor-web-logo-plus.jpg" width="100%" alt="logo"></div>
-          <div class="text">
-            <div class="text-center">shneurweb</div>
-            <div class="text-center">
-              <span class="star"></span>
-              <span class="star"></span>
-              <span class="star"></span>
-              <span class="star"></span>
-              <span class="star"></span>
-            </div>
-          </div>
-        </div>
+<div id="carousel-example-generic" class="carousel" data-ride="">
+  <div class="col-xs-12 no-padding clearfix">
+    <div class="col-xs-1 relative">
+      <!--    <div class="arrow first_arrow disabled"></div>-->
+      <a class="left carousel-control" href="#carousel-example-generic" data-slide="prev">
+        <span class="glyphicon glyphicon-chevron-left"></span>
+      </a>
+    </div>
+
+    <div class="col-xs-10 no-padding">
+      <div class="carousel-inner">
+        <?php print $providers; ?>
       </div>
     </div>
-    <div class="row padding-top-30">
-      <div class="col-xs-2">
-        <div class="service_item">
-          <div class="overflow height_100"><img src="https://lh6.googleusercontent.com/-ehecd-e2r1g/UsyI-rezM1I/AAAAAAAAAE8/Oma14zsJw1w/s0-d/shneor-web-logo-plus.jpg" width="100%" alt="logo"></div>
-          <div class="text">
-            <div class="text-center">shneurweb</div>
-            <div class="text-center">
-              <span class="star"></span>
-              <span class="star"></span>
-              <span class="star"></span>
-              <span class="star"></span>
-              <span class="star"></span>
-            </div>
-          </div>
-        </div>
-      </div>
-      <div class="col-xs-2">
-        <div class="service_item">
-          <div class="overflow height_100"><img src="https://lh6.googleusercontent.com/-ehecd-e2r1g/UsyI-rezM1I/AAAAAAAAAE8/Oma14zsJw1w/s0-d/shneor-web-logo-plus.jpg" width="100%" alt="logo"></div>
-          <div class="text">
-            <div class="text-center">shneurweb</div>
-            <div class="text-center">
-              <span class="star"></span>
-              <span class="star"></span>
-              <span class="star"></span>
-              <span class="star"></span>
-              <span class="star"></span>
-            </div>
-          </div>
-        </div>
-      </div>
-      <div class="col-xs-2">
-        <div class="service_item">
-          <div class="overflow height_100"><img src="https://lh6.googleusercontent.com/-ehecd-e2r1g/UsyI-rezM1I/AAAAAAAAAE8/Oma14zsJw1w/s0-d/shneor-web-logo-plus.jpg" width="100%" alt="logo"></div>
-          <div class="text">
-            <div class="text-center">shneurweb</div>
-            <div class="text-center">
-              <span class="star"></span>
-              <span class="star"></span>
-              <span class="star"></span>
-              <span class="star"></span>
-              <span class="star"></span>
-            </div>
-          </div>
-        </div>
-      </div>
-      <div class="col-xs-2">
-        <div class="service_item">
-          <div class="overflow height_100"><img src="https://lh6.googleusercontent.com/-ehecd-e2r1g/UsyI-rezM1I/AAAAAAAAAE8/Oma14zsJw1w/s0-d/shneor-web-logo-plus.jpg" width="100%" alt="logo"></div>
-          <div class="text">
-            <div class="text-center">shneurweb</div>
-            <div class="text-center">
-              <span class="star"></span>
-              <span class="star"></span>
-              <span class="star"></span>
-              <span class="star"></span>
-              <span class="star"></span>
-            </div>
-          </div>
-        </div>
-      </div>
-      <div class="col-xs-2">
-        <div class="service_item">
-          <div class="overflow height_100"><img src="https://lh6.googleusercontent.com/-ehecd-e2r1g/UsyI-rezM1I/AAAAAAAAAE8/Oma14zsJw1w/s0-d/shneor-web-logo-plus.jpg" width="100%" alt="logo"></div>
-          <div class="text">
-            <div class="text-center">shneurweb</div>
-            <div class="text-center">
-              <span class="star"></span>
-              <span class="star"></span>
-              <span class="star"></span>
-              <span class="star"></span>
-              <span class="star"></span>
-            </div>
-          </div>
-        </div>
-      </div>
-      <div class="col-xs-2">
-        <div class="service_item">
-          <div class="overflow height_100"><img src="https://lh6.googleusercontent.com/-ehecd-e2r1g/UsyI-rezM1I/AAAAAAAAAE8/Oma14zsJw1w/s0-d/shneor-web-logo-plus.jpg" width="100%" alt="logo"></div>
-          <div class="text">
-            <div class="text-center">shneurweb</div>
-            <div class="text-center">
-              <span class="star"></span>
-              <span class="star"></span>
-              <span class="star"></span>
-              <span class="star"></span>
-              <span class="star"></span>
-            </div>
-          </div>
-        </div>
-      </div>
+
+    <div class="col-xs-1 clearfix">
+      <!--    <div class="arrow second_arrow"></div>-->
+      <a class="right carousel-control" href="#carousel-example-generic" data-slide="next">
+        <span class="glyphicon glyphicon-chevron-right"></span>
+      </a>
     </div>
-  </div>
-  <div class="col-xs-1 clearfix second">
-    <div class="arrow second_arrow"></div>
   </div>
 </div>
