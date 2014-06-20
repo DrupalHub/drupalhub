fc5c4f8f6bb49a45e8e38465df9d2786060640bb
Roy Segall <roy.segall@gizra.com>
2014-06-21 01:25:28 +0300

    Commit work.

---
+++ drupalhub/modules/drupalhub/drupalhub_pages/plugins/content_types/service_provider/templates/service-provider.tpl.php
@@ -0,0 +1,8 @@
+<div class="service_item">
+  <div class="image">
+    <?php print $image; ?>
+  </div>
+  <div class="text">
+    <div class="text-center"><?php print $title; ?></div>
+  </div>
+</div>
