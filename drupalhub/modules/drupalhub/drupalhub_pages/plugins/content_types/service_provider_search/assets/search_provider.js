2620d64e77c0163b59111df22c17bbc41a769212
Roy Segall <roy.segall@gizra.com>
2014-06-21 17:50:47 +0300

    Fixing the seracher.

--- drupalhub/modules/drupalhub/drupalhub_pages/plugins/content_types/service_provider_search/assets/search_provider.js
+++ drupalhub/modules/drupalhub/drupalhub_pages/plugins/content_types/service_provider_search/assets/search_provider.js
@@ -5,16 +5,24 @@
     attach: function (context, settings) {
       $("#edit-search").keyup(function(event) {
         $('.carousel').html('<div class="ajax-progress"><span class="throbber"></span></div>');
-        var request = $.ajax({
-          url: settings.baseURL + "service_provider/" + $(this).val()
-        });
+
+        if ($(this).val() == null) {
+          request = $.ajax({
+            url: settings.baseURL + "service_provider"
+          });
+
+        }
+        else {
+          request = $.ajax({
+            url: settings.baseURL + "service_provider/" + $(this).val()
+          });
+        }
 
         request.fail(function() {
           $('.carousel').html(Drupal.t('Service provider was not found.'));
         });
 
         request.done(function(result) {
-          console.log(result);
           $('.carousel').html(result);
         });
       });
