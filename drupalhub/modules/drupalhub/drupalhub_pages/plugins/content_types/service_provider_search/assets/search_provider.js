975ed21f6836bfc3a60876bb99c7d8de20ef81f0
Roy Segall <roy.segall@gizra.com>
2014-06-21 17:43:28 +0300

    Adding missing files.

--- drupalhub/modules/drupalhub/drupalhub_pages/plugins/content_types/service_provider_search/assets/search_provider.js
+++ drupalhub/modules/drupalhub/drupalhub_pages/plugins/content_types/service_provider_search/assets/search_provider.js
@@ -2,9 +2,21 @@
 (function ($) {
 
   Drupal.behaviors.DrupalHubPagesSerachServiceProvider = {
-    attach: function (context) {
-      $("#edit-search").keypress(function() {
-        alert('a');
+    attach: function (context, settings) {
+      $("#edit-search").keyup(function(event) {
+        $('.carousel').html('<div class="ajax-progress"><span class="throbber"></span></div>');
+        var request = $.ajax({
+          url: settings.baseURL + "service_provider/" + $(this).val()
+        });
+
+        request.fail(function() {
+          $('.carousel').html(Drupal.t('Service provider was not found.'));
+        });
+
+        request.done(function(result) {
+          console.log(result);
+          $('.carousel').html(result);
+        });
       });
     }
   };
