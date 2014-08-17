c75a14feaca31d6e073ed4b6dccda4fb76728305
Roy Segall <roy.segall@gizra.com>
2014-08-17 23:59:44 +0300

    Work on the CSS and adjust to small divices.

--- drupalhub/themes/drupalhub/templates/page.tpl.php
+++ drupalhub/themes/drupalhub/templates/page.tpl.php
@@ -76,65 +76,68 @@
 <div class="top-navbar no_border hidden-xs">
   <div class="navbar-inner">
     <div class="container">
-      <div class="nav-collapsee collapsee">
-        <ul class="nav navbar-nav">
-          <li class="search"><a><input type="text" placeholder="<?php print t('Search'); ?>" class="search _w240"></a></li>
-          <li class="social">
-            <a class="youtube" href="<?php print $social['youtube']; ?>"><i class="fa fa-youtube-play"></i></a>
-            <a class="twitter" href="<?php print $social['twitter']; ?>"><i class="fa fa-twitter"></i></a>
-            <a class="google" href="<?php print $social['google']; ?>"><i class="fa fa-google-plus"></i></a>
-            <a class="facebook" href="<?php print $social['facebook']; ?>"><i class="fa fa-facebook-square"></i></a>
-            <a class="github" href="<?php print $social['github']; ?>"><i class="fa fa-github-alt"></i></a>
-          </li>
-        </ul>
+      <div class="row">
+        <div class="nav-collapsee collapsee col-md-12">
+          <ul class="nav navbar-nav">
+            <li class="search"><a><input type="text" placeholder="<?php print t('Search'); ?>" class="search _w240"></a></li>
+            <li class="social">
+              <a class="youtube" href="<?php print $social['youtube']; ?>"><i class="fa fa-youtube-play"></i></a>
+              <a class="twitter" href="<?php print $social['twitter']; ?>"><i class="fa fa-twitter"></i></a>
+              <a class="google" href="<?php print $social['google']; ?>"><i class="fa fa-google-plus"></i></a>
+              <a class="facebook" href="<?php print $social['facebook']; ?>"><i class="fa fa-facebook-square"></i></a>
+              <a class="github" href="<?php print $social['github']; ?>"><i class="fa fa-github-alt"></i></a>
+            </li>
+          </ul>
+        </div>
       </div>
     </div>
   </div>
 </div>
 <header id="navbar" role="banner" class="<?php print $navbar_classes; ?>">
   <div class="container">
-    <div class="navbar-header">
-      <?php if ($logo): ?>
-      <a class="logo navbar-btn pull-left" href="<?php print $front_page; ?>" title="<?php print t('Home'); ?>">
-        <img src="<?php print $logo; ?>" alt="<?php print t('Home'); ?>" />
-      </a>
-      <?php endif; ?>
+    <div class="row">
+      <div class="navbar-header col-md-11">
+        <?php if ($logo): ?>
+        <a class="logo navbar-btn pull-left" href="<?php print $front_page; ?>" title="<?php print t('Home'); ?>">
+          <img src="<?php print $logo; ?>" alt="<?php print t('Home'); ?>" />
+        </a>
+        <?php endif; ?>
 
-      <?php if (!empty($site_name)): ?>
-      <a class="name navbar-brand" href="<?php print $front_page; ?>" title="<?php print t('Home'); ?>"><?php print $site_name; ?></a>
-      <?php endif; ?>
+        <?php if (!empty($site_name)): ?>
+        <a class="name navbar-brand" href="<?php print $front_page; ?>" title="<?php print t('Home'); ?>"><?php print $site_name; ?></a>
+        <?php endif; ?>
 
-      <!-- .btn-navbar is used as the toggle for collapsed navbar content -->
-      <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
-        <span class="sr-only">Toggle navigation</span>
-        <span class="icon-bar"></span>
-        <span class="icon-bar"></span>
-        <span class="icon-bar"></span>
-      </button>
-    </div>
-
-    <?php if (!empty($primary_nav) || !empty($secondary_nav) || !empty($page['navigation']) || user_is_anonymous()): ?>
-      <div class="navbar-collapse collapse">
-        <nav role="navigation">
-          <?php if (!empty($primary_nav)): ?>
-            <?php print render($primary_nav); ?>
-          <?php endif; ?>
-          <?php if (!empty($secondary_nav)): ?>
-            <?php print render($secondary_nav); ?>
-          <?php endif; ?>
-          <?php if (!empty($page['navigation'])): ?>
-            <?php print render($page['navigation']); ?>
-          <?php endif; ?>
-          <?php if (user_is_anonymous()): ?>
-            <?php print $loggin_button; ?>
-          <?php endif; ?>
-        </nav>
+        <!-- .btn-navbar is used as the toggle for collapsed navbar content -->
+        <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
+          <span class="sr-only">Toggle navigation</span>
+          <span class="icon-bar"></span>
+          <span class="icon-bar"></span>
+          <span class="icon-bar"></span>
+        </button>
       </div>
-    <?php endif; ?>
+
+      <?php if (!empty($primary_nav) || !empty($secondary_nav) || !empty($page['navigation']) || user_is_anonymous()): ?>
+        <div class="navbar-collapse collapse col-md-1">
+          <nav role="navigation">
+            <?php if (!empty($primary_nav)): ?>
+              <?php print render($primary_nav); ?>
+            <?php endif; ?>
+            <?php if (!empty($secondary_nav)): ?>
+              <?php print render($secondary_nav); ?>
+            <?php endif; ?>
+            <?php if (!empty($page['navigation'])): ?>
+              <?php print render($page['navigation']); ?>
+            <?php endif; ?>
+            <?php if (user_is_anonymous()): ?>
+              <?php print $loggin_button; ?>
+            <?php endif; ?>
+          </nav>
+        </div>
+      <?php endif; ?>
+    </div>
 </header>
 <?php print $slide_show; ?>
 <div class="main-container container">
-
   <header role="banner" id="page-header">
     <?php if (!empty($site_slogan)): ?>
       <p class="lead"><?php print $site_slogan; ?></p>
