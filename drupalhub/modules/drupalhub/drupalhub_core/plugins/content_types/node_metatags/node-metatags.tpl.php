ca04b43b385cee526f15d0558a9c79efe717c773
Roy Segall <roy.segall@gizra.com>
2014-08-22 20:47:32 +0300

    Finish working on presenting the question.

--- drupalhub/modules/drupalhub/drupalhub_core/plugins/content_types/node_metatags/node-metatags.tpl.php
+++ drupalhub/modules/drupalhub/drupalhub_core/plugins/content_types/node_metatags/node-metatags.tpl.php
@@ -1,3 +1,9 @@
+<?php if($vote): ?>
+  <?php print $vote; ?>
+<?php endif; ?>
 <i class="fa fa-clock-o"></i> <?php print $time; ?>
 <i class="fa fa-user"></i> <?php print $author; ?>
+<?php if($follow): ?>
+  <?php print $follow; ?>
+<?php endif; ?>
 <i class="fa fa-tags"></i> <?php print $tags; ?>
--- drupalhub/modules/drupalhub/drupalhub_core/plugins/widgets/drupalhub_vud/widget.tpl.php
+++ drupalhub/modules/drupalhub/drupalhub_core/plugins/widgets/drupalhub_vud/widget.tpl.php
@@ -10,11 +10,11 @@
 ?>
 
 <div class="vud-widget vud-widget-upanddown" id="<?php print $id; ?>">
-  <span class="points"><?php print $points; ?></span>
-
   <?php if($show_up_as_link): ?>
-    <span class="points">|</span> <a href="<?php print $link_up; ?>" class="<?php print $link_class_up; ?>"><i class="fa vote fa-thumbs-o-up"></i></a>
+    <a href="<?php print $link_up; ?>" class="<?php print $link_class_up; ?>"><i class="fa vote fa-thumbs-o-up"></i></a>
   <? else: ?>
-    <span class="points">|</span> <a href="<?php print $link_down; ?>" class="<?php print $link_class_down; ?>"><i class="fa vote fa-thumbs-o-down"></i></a>
+    <a href="<?php print $link_down; ?>" class="<?php print $link_class_down; ?>"><i class="fa vote fa-thumbs-o-down"></i></a>
   <?php endif; ?>
+
+  <span class="points"><?php print $points; ?></span>
 </div>
