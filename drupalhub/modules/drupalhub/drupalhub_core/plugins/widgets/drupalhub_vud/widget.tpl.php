eb2b865a80f005bcf92cf683f7d31c65ccb8d61f
Roy Segall <roy.segall@gizra.com>
2014-08-22 13:42:08 +0300

    Finish with the up down widget.

--- drupalhub/modules/drupalhub/drupalhub_core/plugins/widgets/drupalhub_vud/widget.tpl.php
+++ drupalhub/modules/drupalhub/drupalhub_core/plugins/widgets/drupalhub_vud/widget.tpl.php
@@ -7,14 +7,14 @@
  * UpAndDown widget theme for Vote Up/Down
  * TODO use $show_up_as_link and $show_down_as_link
  */
-
-dpm(get_defined_vars());
 ?>
 
-<span class="points"><?php print $points; ?></span>
+<div class="vud-widget vud-widget-upanddown clearfix" id="<?php print $id; ?>">
+  <span class="points"><?php print $points; ?></span>
 
-<?php if($show_up_as_link): ?>
-  <span class="points">|</span> <i class="fa vote fa-thumbs-o-up"></i>
-<? else: ?>
-  <span class="points">|</span> <i class="fa vote fa-thumbs-o-down"></i>
-<?php endif; ?>
+  <?php if($show_up_as_link): ?>
+    <span class="points">|</span> <a href="<?php print $link_up; ?>" class="<?php print $link_class_up; ?>"><i class="fa vote fa-thumbs-o-up"></i></a>
+  <? else: ?>
+    <span class="points">|</span> <a href="<?php print $link_down; ?>" class="<?php print $link_class_down; ?>"><i class="fa vote fa-thumbs-o-down"></i></a>
+  <?php endif; ?>
+</div>
