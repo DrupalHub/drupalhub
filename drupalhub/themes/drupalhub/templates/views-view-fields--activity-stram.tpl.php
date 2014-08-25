66d1ca7a389c0d1ad69c5f0c60a4a29de03e8182
Roy Segall <roy.segall@gizra.com>
2014-08-25 21:27:03 +0300

    Adding the icons.

--- drupalhub/themes/drupalhub/templates/views-view-fields--activity-stram.tpl.php
+++ drupalhub/themes/drupalhub/templates/views-view-fields--activity-stram.tpl.php
@@ -1,4 +1,10 @@
 <div class="row activity-stream-instance <?php print $fields['type']->raw; ?>">
+  <div class="icons">
+    <i class="fa fa-thumbs-up disabled"></i>
+    <i class="fa fa-drupal disabled"></i>
+    <i class="fa fa-comment disabled"></i>
+    <i class="fa fa-pencil disabled"></i>
+  </div>
   <div class="activity-header">
     <div class="picture-name"><?php print $fields['picture']->content; ?> <?php print $fields['name']->content; ?></div>
     <?php print $fields['timestamp']->content; ?>
