fd92c4f64b0b62d7cd572753c8e8d27193c4ed42
Roy Segall <roy.segall@gizra.com>
2014-08-28 00:46:58 +0300

    Add more style to the user page.

--- drupalhub/modules/drupalhub/drupalhub_core/plugins/layouts/drupalhub_user/drupalhub-user.tpl.php
+++ drupalhub/modules/drupalhub/drupalhub_core/plugins/layouts/drupalhub_user/drupalhub-user.tpl.php
@@ -3,7 +3,7 @@
 </div>
 <div class="<?php print $classes ?>" <?php if (!empty($css_id)) { print "id=\"$css_id\""; } ?>>
   <div class="row">
-    <div class="col-md-12">
+    <div class="top col-md-12">
       <?php print $content['top']; ?>
     </div>
   </div>
