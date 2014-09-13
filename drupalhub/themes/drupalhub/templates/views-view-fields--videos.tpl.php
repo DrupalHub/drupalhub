64f781fc74f133290c0130dd1a2925ba7b335a56
Roy Segall <roy.segall@gizra.com>
2014-09-13 14:33:47 +0300

    Work on the display of the videos pages.

---
+++ drupalhub/themes/drupalhub/templates/views-view-fields--videos.tpl.php
@@ -0,0 +1,11 @@
+<div class="video-wrapper">
+  <div class="video">
+    <?php print $fields['field_address']->content; ?>
+  </div>
+  <div class="information">
+    <?php print $fields['title']->content; ?>
+    <div class="user"><?php print $fields['name']->content; ?></div>
+    <div class="length"><?php print $fields['youtube_data']->content; ?></div>
+    <div class="vote"><?php print $fields['field_vote']->content; ?></div>
+  </div>
+</div>
