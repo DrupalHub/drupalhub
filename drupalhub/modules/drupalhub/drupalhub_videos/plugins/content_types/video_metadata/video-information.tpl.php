1f5e97a0056ba5f795b9f4712736b2b54606bc4d
Roy Segall <roy.segall@gizra.com>
2014-09-13 16:31:26 +0300

    Working on the page of a single video.

---
+++ drupalhub/modules/drupalhub/drupalhub_videos/plugins/content_types/video_metadata/video-information.tpl.php
@@ -0,0 +1,8 @@
+<h1><?php print $title; ?></h1>
+<div class="info">
+  <div class="duration"> <i class="fa fa-clock-o"></i> <?php print $duration; ?></div>
+  <div class="author"><i class="fa fa-user"></i> <?php print $author; ?></div>
+  <div class="uploader"><i class="fa fa-youtube-play"></i> <?php print $uploader; ?></div>
+  <div class="views"><i class="fa fa-play"></i> <?php print $views; ?></div>
+</div>
+<div class="description"><?php print $description; ?></div>
