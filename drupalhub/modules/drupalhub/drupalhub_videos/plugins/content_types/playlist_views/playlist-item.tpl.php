e7c08a62440c5ec5a94f847aa72bc2db94d48443
Roy Segall <roy.segall@gizra.com>
2014-09-21 23:57:24 +0300

    Display playlist.

---
+++ drupalhub/modules/drupalhub/drupalhub_videos/plugins/content_types/playlist_views/playlist-item.tpl.php
@@ -0,0 +1,7 @@
+<div class="video-wrapper clearfix">
+  <div class="image"><?php print $image; ?></div>
+  <div class="info">
+    <p class="title"><?php print $label; ?></p>
+    <p class="length"><i class="fa fa-clock-o"></i> <?php print $length; ?></p>
+  </div>
+</div>
