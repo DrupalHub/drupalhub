7e70c7242cdaea76c28400c127e2dc40e4e63dca
Roy Segall <roy.segall@gizra.com>
2014-09-26 01:29:57 +0300

    Start working with the update of the field.

--- drupalhub/modules/drupalhub/drupalhub_users/plugins/content_types/about/about.js
+++ drupalhub/modules/drupalhub/drupalhub_users/plugins/content_types/about/about.js
@@ -5,8 +5,9 @@
       $("#UpdateAbout .modal-footer button").click(function() {
 
         $.DrupalHubFormInit('UpdateAbout');
+        var about = $("#UpdateAbout #about").val();
 
-        if ($("#UpdateAbout #about").val() == "") {
+        if (about == "") {
           $("#UpdateAbout #about").SetError(Drupal.t('This is required field'));
         }
 
@@ -16,13 +17,16 @@
 
         $(this).AddSpinner();
 
-//        jQuery.DrupalHubAjax('PATCH', "api/v1/update_about", {
-//          about: 'a'
-//        })
-//        .success(function(result) {
-//          console.log(result);
-//          jQuery.RemoveSpinner();
-//        });
+        jQuery.DrupalHubAjax('PATCH', "api/v1/update_about", {
+          about: about
+        })
+          .error(function(result) {
+            console.log(result);
+          })
+        .success(function(result) {
+          console.log(result);
+          jQuery.RemoveSpinner();
+        });
       });
     }
   };
