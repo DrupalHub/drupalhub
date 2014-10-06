30f21df0bec1e82dc865059b51e68d5f73df1cad
Roy Segall <roy.segall@gizra.com>
2014-10-07 00:00:08 +0300

    Finish working with the type ahead.

--- drupalhub/modules/drupalhub/drupalhub_wiki/plugins/restful/node/wiki/1.0/DrupalHubWiki.class.php
+++ drupalhub/modules/drupalhub/drupalhub_wiki/plugins/restful/node/wiki/1.0/DrupalHubWiki.class.php
@@ -1,4 +1,15 @@
 <?php
 
 class DrupalHubWiki extends RestfulEntityBaseNode {
+
+  public function publicFieldsInfo() {
+    $public_fields = parent::publicFieldsInfo();
+
+    $public_fields['category'] = array(
+      'property' => 'field_category',
+      'sub_property' => 'name',
+    );
+
+    return $public_fields;
+  }
 }
