90f87802a24843ed58b2a323ed1631909ae64d06
Roy Segall <roy.segall@gizra.com>
2014-10-11 02:27:13 +0300

    Add more extensive test suites.

--- drupalhub/behat/features/bootstrap/FeatureContext.php
+++ drupalhub/behat/features/bootstrap/FeatureContext.php
@@ -13,6 +13,8 @@ use Drupal\DrupalExtension\Context\DrupalContext;
  */
 class FeatureContext extends DrupalContext {
 
+  protected $DrupalHubUsers = array();
+
   /**
    * Initializes context.
    * Every scenario gets its own context object.
@@ -20,6 +22,8 @@ class FeatureContext extends DrupalContext {
    * @param array $parameters context parameters (set them up through behat.yml)
    */
   public function __construct(array $parameters) {
+    $this->DrupalHubUsers = $parameters['users'];
+
   }
 
   /**
@@ -28,5 +32,35 @@ class FeatureContext extends DrupalContext {
   public function iShouldPrintPage() {
     $page = $this->getSession();
     print_r($page->getPage()->getContent());
+  }
+
+  /**
+   * @Given /^I am logging in as "([^"]*)"$/
+   */
+  public function iAmLoggingInAs($user) {
+    // Check if logged in.
+    if ($this->loggedIn()) {
+      $this->logout();
+    }
+
+    if (empty($this->DrupalHubUsers[$user])) {
+      throw new \Exception(sprintf('The user name was not found', $user));
+    }
+
+    $this->getSession()->visit($this->locatePath('/user'));
+    $element = $this->getSession()->getPage();
+    $element->fillField('name', $user);
+    $element->fillField('pass', $this->DrupalHubUsers[$user]);
+    $submit = $element->findButton($this->getDrupalText('log_in'));
+    if (empty($submit)) {
+      throw new \Exception(sprintf("No submit button at %s", $this->getSession()->getCurrentUrl()));
+    }
+
+    // Log in.
+    $submit->click();
+
+    if (!$this->loggedIn()) {
+      throw new \Exception(sprintf("Failed to log in as user '%s' with role '%s'", $this->user->name, $this->user->role));
+    }
   }
 }
