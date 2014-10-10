8ff76d9da94686a64ac8e9004754ef2159cdd568
Roy Segall <roy.segall@gizra.com>
2014-10-10 23:57:01 +0300

    #62: Adding behat tests.

---
+++ drupalhub/behat/features/bootstrap/FeatureContext.php
@@ -0,0 +1,32 @@
+<?php
+
+use Behat\Behat\Context\ClosuredContextInterface,
+    Behat\Behat\Context\TranslatedContextInterface,
+    Behat\Behat\Context\BehatContext,
+    Behat\Behat\Exception\PendingException;
+use Behat\Gherkin\Node\PyStringNode,
+    Behat\Gherkin\Node\TableNode;
+use Drupal\DrupalExtension\Context\DrupalContext;
+
+/**
+ * Features context.
+ */
+class FeatureContext extends DrupalContext {
+
+  /**
+   * Initializes context.
+   * Every scenario gets its own context object.
+   *
+   * @param array $parameters context parameters (set them up through behat.yml)
+   */
+  public function __construct(array $parameters) {
+  }
+
+  /**
+   * @Given /^I should print page$/
+   */
+  public function iShouldPrintPage() {
+    $page = $this->getSession();
+    print_r($page->getPage()->getContent());
+  }
+}
