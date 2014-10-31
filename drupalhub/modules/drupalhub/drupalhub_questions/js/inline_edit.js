b5fab980677f2797921c408e10aefd157e5c4871
Roy Segall <roy.segall@gizra.com>
2014-10-31 17:51:11 +0200

    Work on the effect of the replacing.

--- drupalhub/modules/drupalhub/drupalhub_questions/js/inline_edit.js
+++ drupalhub/modules/drupalhub/drupalhub_questions/js/inline_edit.js
@@ -2,10 +2,26 @@
 
   Drupal.behaviors.InlineEditTitle = {
     attach: function() {
-      $(".pane-node-title h1").dblclick(function() {
-        alert('a');
+      $(".pane-node-title h1").live('dblclick', function() {
+        var title = $(this).text();
+        $(this).parent().html(
+          '<input class="form-control" id="title" type="text" value="' + title + '" /> ' +
+          '<button type="button" id="update_title" class="btn btn-primary">' + Drupal.t('Update') + '</button>');
       });
     }
   };
+
+  Drupal.behaviors.SendInlineEdititng = {
+    attach: function() {
+      $("#update_title").live('click', function() {
+        $(this).attr("class", "btn btn-success");
+        $(this).html('Saved!');
+
+        $.DrupalHubSleep(3);
+
+        $(".pane-node-title").html("<h1>" + $("#title").val() + "</h1>");
+      });
+    }
+  }
 
 })(jQuery);
