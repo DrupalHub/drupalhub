82e76e69c9940e84b262a75844a790cab48014f8
Roy Segall <roy.segall@gizra.com>
2014-11-01 17:12:32 +0200

    Commit work.

--- drupalhub/modules/drupalhub/drupalhub_questions/js/inline_edit.js
+++ drupalhub/modules/drupalhub/drupalhub_questions/js/inline_edit.js
@@ -1,5 +1,8 @@
 (function($) {
 
+  /**
+   * Replacing the title with form for a quick edit.
+   */
   Drupal.behaviors.InlineEditTitle = {
     attach: function() {
       if (Drupal.settings.drupalhub_question.access == false) {
@@ -15,6 +18,9 @@
     }
   };
 
+  /**
+   * Submitting the title quick edit form.
+   */
   Drupal.behaviors.SendInlineEdititng = {
     attach: function() {
       $(".pane-node-title").on('click', "#update_title", function() {
@@ -41,6 +47,10 @@
     }
   };
 
+  /**
+   * Replace the body with a CKEDITOR instance for a quick edit of the question
+   * body.
+   */
   Drupal.behaviors.InlineBodyEditing = {
     attach: function() {
       if (Drupal.settings.drupalhub_question.access == false) {
@@ -56,6 +66,9 @@
     }
   };
 
+  /**
+   * Submitting the CKEDITOR form.
+   */
   Drupal.behaviors.InlineBodyEditingSubmitting = {
     attach: function() {
       $(".panel-col-top .field-name-body").on('click', "#update_body", function() {
@@ -81,5 +94,43 @@
       });
     }
   };
+
+  /**
+   * Quick edit for the question's terms.
+   */
+  Drupal.behaviors.TermsInlineEdit = {
+    attach: function() {
+      if (Drupal.settings.drupalhub_question.access == false) {
+        return;
+      }
+      
+      $(".pane-node-metatags .fa-tags").on('dblclick', function() {
+        var tags = $(".tags-links").text();
+
+        BootstrapDialog.show({
+          message: '<input class="form-control" id="tags" type="text" value="' + tags + '" />',
+          buttons: [
+            {
+              icon: 'glyphicon glyphicon-remove',
+              label: ' ' + Drupal.t('Close'),
+              cssClass: 'btn-danger',
+              action: function(dialogItself) {
+                dialogItself.close();
+              }
+            },
+            {
+              icon: 'glyphicon glyphicon-ok',
+              label: ' ' + Drupal.t('Send'),
+              cssClass: 'btn-success',
+              action: function(dialogItself) {
+                alert('Sent!');
+                dialogItself.close();
+              }
+            }
+          ]
+        });
+      });
+    }
+  }
 
 })(jQuery);
