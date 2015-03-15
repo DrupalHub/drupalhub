d43adfa28caa689ba8903e8e9252361e6ca1cf71
Roy Segall <roy.segall@gizra.com>
2015-03-15 21:57:47 +0200

    Adding tags endpoint.

--- drupalhub/modules/drupalhub/drupalhub_restful/plugins/restful/taxonomy_term/tags/DrupalHubTags.class.php
+++ drupalhub/modules/drupalhub/drupalhub_restful/plugins/restful/taxonomy_term/tags/DrupalHubTags.class.php
@@ -1,4 +1,27 @@
 <?php
 
 class DrupalHubTags extends RestfulEntityBaseTaxonomyTerm {
+
+  /**
+   * {@inheritdoc}
+   */
+  public function publicFieldsInfo() {
+    $public_fields = parent::publicFieldsInfo();
+
+    $public_fields['url'] = array(
+      'property' => 'tid',
+      'process_callbacks' => array(
+        array($this, 'processUrl'),
+      ),
+    );
+    return $public_fields;
+  }
+
+  /**
+   * Render the item.
+   */
+  public function processUrl($tid) {
+    return 'tags.html/#' . $tid;
+  }
+
 }
