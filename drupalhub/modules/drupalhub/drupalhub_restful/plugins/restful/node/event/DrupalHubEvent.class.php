ec9fccdfae6fdffac7236aedf78b794d292f3e3f
Roy Segall <roy.segall@gizra.com>
2015-03-15 23:41:07 +0200

    Done with the events end point.

--- drupalhub/modules/drupalhub/drupalhub_restful/plugins/restful/node/event/DrupalHubEvent.class.php
+++ drupalhub/modules/drupalhub/drupalhub_restful/plugins/restful/node/event/DrupalHubEvent.class.php
@@ -30,21 +30,25 @@ class DrupalHubEvent extends \DrupalHubRestfulNode {
     );
 
     $public_fields['google_address'] = array(
-      'property' => 'field_date',
-      'sub_property' => 'value2',
+      'property' => 'field_location',
       'process_callbacks' => array(
-        array($this, 'processDate'),
+        array($this, 'processGoogleAddress'),
       ),
     );
 
     $public_fields['human_address'] = array(
-      'property' => 'field_date',
-      'sub_property' => 'value2',
-      'process_callbacks' => array(
-        array($this, 'processDate'),
-      ),
+      'property' => 'field_human_address',
     );
 
+    $public_fields['latitude'] = array(
+      'property' => 'field_location',
+      'sub_property' => 'lat',
+    );
+
+    $public_fields['longitude'] = array(
+      'property' => 'field_location',
+      'sub_property' => 'lng',
+    );
 
     return $public_fields;
   }
@@ -66,5 +70,62 @@ class DrupalHubEvent extends \DrupalHubRestfulNode {
    */
   protected function processDate($value) {
     return date('d/m/Y H:i', $value);
+  }
+
+  protected function processGoogleAddress($location) {
+    $Maps = new DrupalHubGoogleMapsApi($location['lat'], $location['lng']);
+    return $Maps->GetAddress('formatted_address');
+  }
+
+  /**
+   * Get all the RSVP of the event.
+   */
+  protected function processRSVP($nid) {
+    return array(
+      'coming' => $this->queryEventRSVP('coming', $nid),
+      'maybe' => $this->queryEventRSVP('maybe', $nid),
+      'not' => $this->queryEventRSVP('not', $nid),
+    );
+  }
+
+  /**
+   * Get the RSVP users that their RSVP match to a given status.
+   *
+   * @param $status
+   *   The status of the RSVP: yes, no or maybe.
+   * @param $nid
+   *   The event NID.
+   *
+   * @return array
+   *   Array of users and their image.
+   */
+  private function queryEventRSVP($status, $nid) {
+    $query = new EntityFieldQuery();
+
+    $results = $query
+      ->entityCondition('entity_type', 'node')
+      ->propertyCondition('type', 'attendees')
+      ->fieldCondition('field_node_reference', 'target_id', $nid)
+      ->fieldCondition('field_status', 'value', $status)
+      ->execute();
+
+    if (empty($results['node'])) {
+      return;
+    }
+
+    $nodes = node_load_multiple(array_keys($results['node']));
+
+    $users = array();
+    foreach ($nodes as $node) {
+      $wrapper = entity_metadata_Wrapper('node', $node);
+
+      $author = $wrapper->field_user_reference;
+      $users[] = array(
+        'name' => $author->label(),
+        'image' => $this->authorImage($author->value()),
+      );
+    }
+
+    return $users;
   }
 }
