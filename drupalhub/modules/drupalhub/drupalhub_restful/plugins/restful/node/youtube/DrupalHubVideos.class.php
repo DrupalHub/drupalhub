92fd7d8f399a103cc175ae9d852166099c9af190
Roy Segall <roy.segall@gizra.com>
2015-03-17 01:39:23 +0200

    Another end point.

--- drupalhub/modules/drupalhub/drupalhub_restful/plugins/restful/node/youtube/DrupalHubVideos.class.php
+++ drupalhub/modules/drupalhub/drupalhub_restful/plugins/restful/node/youtube/DrupalHubVideos.class.php
@@ -13,26 +13,42 @@ class DrupalHubVideos extends \DrupalHubRestfulNode {
   public function publicFieldsInfo() {
     $public_fields = parent::publicFieldsInfo();
 
+    $public_fields['duration'] = array(
+      'property' => 'field_address',
+      'process_callbacks' => array(
+        array($this, 'processDuration'),
+      )
+    );
+
+    $public_fields['embed'] = array(
+      'property' => 'field_address',
+      'process_callbacks' => array(
+        array($this, 'processVideoId'),
+      ),
+    );
+
     $public_fields['image'] = array(
       'property' => 'field_address',
       'process_callbacks' => array(
-        array($this, 'imageProcess'),
+        array($this, 'processImage'),
       ),
-    );
-
-    $public_fields['length'] = array(
-      'property' => 'field_address',
-      'process_callbacks' => array(
-        array($this, 'lengthProcess'),
-      ),
-    );
-
-    $public_fields['address'] = array(
-      'property' => 'field_address',
-      'sub_property' => 'video_url',
     );
 
     return $public_fields;
+  }
+
+  protected function processDuration($field_address) {
+    $data = unserialize($field_address['video_data']);
+    return gmdate("H:i:s", $data['media$group']['media$content'][0]['duration']);
+  }
+
+  protected function processVideoId($field_address) {
+    return _video_embed_field_get_youtube_id($field_address['video_url']);
+  }
+
+  protected function processImage($field_address) {
+    $embed = $this->processVideoId($field_address);
+    return "http://img.youtube.com/vi/{$embed}/0.jpg";
   }
 
 }
