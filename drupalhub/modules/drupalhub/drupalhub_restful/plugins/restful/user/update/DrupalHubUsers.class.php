9d7eaf934fb7bd6353ce7ed127a48dd77f19f9e7
Roy Segall <roy.segall@gizra.com>
2015-03-17 10:11:24 +0200

    Start working on the users end point.

--- drupalhub/modules/drupalhub/drupalhub_restful/plugins/restful/user/update/DrupalHubUsers.class.php
+++ drupalhub/modules/drupalhub/drupalhub_restful/plugins/restful/user/update/DrupalHubUsers.class.php
@@ -13,7 +13,53 @@ class DrupalHubUsers extends RestfulEntityBaseUser {
       'sub_property' => 'value',
     );
 
+    $public_fields['image'] = array(
+      'property' => 'uid',
+      'process_callbacks' => array(
+        array($this, 'processImage'),
+      ),
+    );
+
+    $public_fields['company'] = array(
+      'property' => 'field_company',
+      'sub_property' => 'name',
+    );
+
+    $public_fields['reputation'] = array(
+      'property' => 'field_reputation',
+    );
+
+    $public_fields['joined'] = array(
+      'property' => 'created',
+      'process_callbacks' => array(
+        array($this, 'processJoined'),
+      ),
+    );
+
+    $public_fields['number'] = array(
+      'property' => 'uid',
+      'process_callbacks' => array(
+        array($this, 'processNumber'),
+      )
+    );
+
     return $public_fields;
+  }
+
+  public function processImage($uid) {
+    $author = user_load($uid);
+    return drupalhub_users_user_picture($author, 'thumbnail');
+  }
+
+  public function processJoined($created) {
+    return date('d/m/Y', $created);
+  }
+
+  public function processNumber($uid) {
+    return array(
+      'questions' => 22,
+      'comments' => 22,
+    );
   }
 
 }
