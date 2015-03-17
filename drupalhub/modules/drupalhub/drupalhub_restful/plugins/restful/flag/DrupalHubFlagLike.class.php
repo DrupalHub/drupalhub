aa989d0ea633d8983ec70d3b676fdffdd49dc7f8
Roy Segall <roy.segall@gizra.com>
2015-03-17 16:07:12 +0200

    Add a flag end point.

---
+++ drupalhub/modules/drupalhub/drupalhub_restful/plugins/restful/flag/DrupalHubFlagLike.class.php
@@ -0,0 +1,77 @@
+<?php
+
+class DrupalHubFlagLike extends \RestfulEntityBase {
+
+  /**
+   * {@inheritdoc}
+   */
+  public function publicFieldsInfo() {
+    $public_fields = parent::publicFieldsInfo();
+
+    $public_fields['entity_type'] = array(
+      'property' => 'entity_type',
+    );
+
+    $public_fields['entity_id'] = array(
+      'property' => 'entity_id',
+    );
+
+    $public_fields['uid'] = array(
+      'property' => 'uid',
+    );
+
+    return $public_fields;
+  }
+
+  /**
+   * Sort the query for list.
+   *
+   * @param \EntityFieldQuery $query
+   *   The query object.
+   *
+   * @throws \RestfulBadRequestException
+   *
+   * @see \RestfulEntityBase::getQueryForList
+   */
+  protected function queryForListSort(\EntityFieldQuery $query) {
+    $query->propertyOrderBy('fid', 'ASC');
+  }
+
+  /**
+   * Set the bundle of the flag as the flag name.
+   */
+  public function entityPreSave(\EntityMetadataWrapper $wrapper) {
+    $wrapper->name->set($this->getBundle());
+  }
+
+  /**
+   * Validating for a single flag.
+   */
+  public function entityValidate(\EntityMetadataWrapper $wrapper) {
+    $request = $this->getRequest();
+    $query = new EntityFieldQuery();
+    $results = $query
+      ->entityCondition('entity_type', 'flagging')
+      ->propertyCondition('entity_type', $request['entity_type'])
+      ->propertyCondition('entity_id', $request['entity_id'])
+      ->propertyCondition('uid', $request['uid'])
+      ->count()
+      ->execute();
+
+    if ($results > 0) {
+      throw new \RestfulBadRequestException('The user already flagged this entity.');
+    }
+
+    parent::entityValidate($wrapper);
+  }
+
+  public function deleteEntity($entity_id) {
+    db_delete('flagging')
+      ->condition('flagging_id', $entity_id)
+      ->execute();
+
+    // Set the HTTP headers.
+    $this->setHttpHeaders('Status', 204);
+  }
+
+}
