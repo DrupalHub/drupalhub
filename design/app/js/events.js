8f6569162a09747138eab8d51bc4218dce013099
Roy Segall <roy.segall@gizra.com>
2015-03-20 21:39:22 +0200

    Finish with the events in the front page.

--- design/app/js/events.js
+++ design/app/js/events.js
@@ -2,8 +2,6 @@ DrupalHub.controller('eventsCtrl', function($scope, DrupalHubRequest, $location)
 
   $scope.events = {};
 
-  DrupalHubRequest.localRequest('event');
-
   DrupalHubRequest.localRequest('get', $location.path() == "" ? 'event?range=10' : 'event').
     success(function(data, status) {
       $scope.events = data.data;
