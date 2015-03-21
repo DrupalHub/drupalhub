bd4ca86dc979bdb114ada08cf26b63ebab3f5488
Roy Segall <roy.segall@gizra.com>
2015-03-21 21:40:10 +0200

    We have comments.

--- design/app/js/Directives/comments.js
+++ design/app/js/Directives/comments.js
@@ -1,9 +1,13 @@
-DrupalHub.directive('drupalHubComments', function($location) {
+DrupalHub.directive('drupalHubComments', function($location, DrupalHubRequest) {
   return {
     restrict: 'AE',
     templateUrl: 'pages/comments.html',
     link: function($scope) {
-      console.log($location);
+      var nid = $location.path().split('/')[2];
+
+      DrupalHubRequest.localRequest('get', 'comments?nid=' + nid).success(function(data, status) {
+        $scope.comments = data.data;
+      });
     }
   };
 });
