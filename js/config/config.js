8ece17fd8e556808cd15d8db21b568f270c381d4
Roy Segall <roy.segall@gizra.com>
2015-04-10 02:27:35 +0300

    Built DrupalHub from commit 0803981 on branch master

--- js/config/config.js
+++ js/config/config.js
@@ -1,7 +1,7 @@
 angular.module('DrupalHubConfig', [])
-  .constant('SERVER', 'http://localhost/drupalhub/www/api/');
 
-// Implements for flag directive the address for endpoint.
-angular.module('flagConfig', []).constant('flagConfig', {
-  'server': 'http://localhost/drupalhub/www/api/'
-});
+.constant('Config', {backend:'http://localhost/drupalhub/www/api/',debugUiRouter:true})
+
+.value('debug', true)
+
+;
---
+++ scripts/config.js
@@ -0,0 +1,5 @@
+angular.module('DrupalHubConfig', [])
+
+.constant('Config', {backend:'http://dev-drupalhub.gotpantheon.com/api/'})
+
+;
