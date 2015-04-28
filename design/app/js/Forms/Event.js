4bd3e6e9d4b375dacfeab3927d11711bd2a738d1
Roy Segall <roy.segall@gizra.com>
2015-04-28 22:30:55 +0300

    Commit js file.

--- design/app/js/Forms/Event.js
+++ design/app/js/Forms/Event.js
@@ -1,2 +1,71 @@
 DrupalHub.controller('EventFormCtrl', function($scope, DrupalHubRequest) {
+
+  $scope.event = {
+    label: '',
+    location: '',
+    date: '',
+    text: ''
+  };
+
+  $scope.submit = function() {
+
+    $scope.errors = [];
+
+    if (!$scope.eventForm.label.$valid) {
+      $scope.errors.push('The label is required field.');
+    }
+
+    if (!$scope.eventForm.location.$valid) {
+      $scope.errors.push('The location is required field.');
+    }
+
+    if (!$scope.eventForm.date.$valid) {
+      $scope.errors.push('The date is required field.');
+    }
+
+    if (!$scope.eventForm.text.$valid) {
+      $scope.errors.push('The text is required field.');
+    }
+
+  };
+
+  $scope.today = function() {
+    $scope.dt = new Date();
+  };
+  $scope.today();
+
+  $scope.clear = function () {
+    $scope.dt = null;
+  };
+
+  // Disable weekend selection
+  $scope.disabled = function(date, mode) {
+    return ( mode === 'day' && ( date.getDay() === 0 || date.getDay() === 6 ) );
+  };
+
+  $scope.toggleMin = function() {
+    $scope.minDate = $scope.minDate ? null : new Date();
+  };
+  $scope.toggleMin();
+
+  $scope.open = function($event) {
+    $event.preventDefault();
+    $event.stopPropagation();
+
+    $scope.opened = true;
+  };
+
+  $scope.dateOptions = {
+    formatYear: 'yy',
+    startingDay: 1,
+    showWeeks:'false',
+    startingDay: 1
+  };
+
+  $scope.pickerPopup = {
+    closeText: 'a'
+  };
+
+  $scope.format = 'dd/MM/yyyy';
+
 });
