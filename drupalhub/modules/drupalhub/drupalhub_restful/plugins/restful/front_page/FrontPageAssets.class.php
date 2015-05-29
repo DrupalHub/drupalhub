fe435f82857686a4f6ab390bd9eb13f857da850f
Roy Segall <roy.segall@gizra.com>
2015-05-30 02:16:01 +0300

    Improve performance of the front page.

---
+++ drupalhub/modules/drupalhub/drupalhub_restful/plugins/restful/front_page/FrontPageAssets.class.php
@@ -0,0 +1,66 @@
+<?php
+
+class FrontPageAssets extends \RestfulBase implements RestfulDataProviderInterface {
+
+  /**
+   * Overrides \RestfulEntityBase::controllers.
+   */
+  protected $controllers = array(
+    '' => array(
+      \RestfulInterface::GET => 'frontPageAsset',
+    ),
+  );
+
+  /**
+   * Return the properties that should be public.
+   *
+   * @throws \RestfulEntityViewMode
+   *
+   * @return array
+   */
+  public function publicFieldsInfo() {
+    return array(
+      'status' => array(),
+    );
+  }
+
+  /**
+   * Generate email for reset the user password.
+   *
+   * @return array
+   * @throws RestfulBadRequestException
+   *
+   * @see user_pass_validate()
+   * @see user_pass_submit()
+   */
+  public function frontPageAsset() {
+    $this->getAccount();
+    return array(
+      'permissions' => $this->getPermission(array(
+        'question_create' => 'create question content',
+        'blog_create' => 'create blog content',
+      )),
+      'content' => array(
+        'question' => $this->getContent('question'),
+        'blog' => $this->getContent('blog'),
+        'videos' => $this->getContent('video'),
+        'events' => $this->getContent('event'),
+      ),
+    );
+  }
+
+  private function getPermission(array $permissions) {
+    foreach ($permissions as &$permission) {
+      $permission = user_access($permission, $this->getAccount());
+    }
+
+    return $permissions;
+  }
+
+  private function getContent($bundle) {
+    $handler = restful_get_restful_handler($bundle);
+    $handler->setRange(5);
+    $list = $handler->getList();
+    return array(($list));
+  }
+}
