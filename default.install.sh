46a99a7c104d4bd579b8a86f0193c51d2492f8cf
Roy Segall <roy.segall@gizra.com>
2015-06-02 19:06:39 +0300

    Updating read me file.

--- default.install.sh
+++ default.install.sh
@@ -24,8 +24,17 @@ cd www
 
 drush si -y drupalhub --account-name=$ADMIN_USERNAME --account-pass=$ADMIN_PASSWORD --account-mail=$ADMIN_EMAIL --db-url=mysql://$MYSQL_USERNAME:$MYSQL_PASSWORD@$MYSQL_HOST/$MYSQL_DB_NAME --uri=$BASE_DOMAIN_URL drupalhub_migrate_content.dummy_content=TRUE
 
-# These commands migrates dummy content and is used for development and testing. Comment out both lines if you wish to have a clean drupalhub installation.
+# These commands migrates dummy content and is used for development and testing.
+# Comment out both lines if you wish to have a clean OpenScholar installation.
 drush mi --all --user=1
 
-# This command does the login for you when the build script is done. It will open a new tab in your default browser and login to your project as the Administrator. Comment out this line if you do not want the login to happen automatically.
-drush uli --uri=$BASE_DOMAIN_URL
+# This command does the login for you when the build script is done. It will
+# open a new tab in your default browser and login to your project as the
+# Administrator. Comment out this line if you do not want the login to happen
+# automatically.
+cd ../design
+
+bundle install
+npm install
+bower install
+grunt serve
--- readme.md
+++ readme.md
@@ -1,3 +1,22 @@
 DrupalHub
 ----------
 ![image](https://travis-ci.org/DrupalHub/drupalhub.svg)
+
+Drupal is a code name for the Drupal community in Israel. The project is fully
+decouple - Drupal serve as the backend while in the front is an Angular JS app.
+
+How to start contribute?
+----------
+
+You need [Drush](http://www.drush.org/en/master/), [npm](https://www.npmjs.com/),
+[grunt](http://gruntjs.com/), [bower](http://bower.io/).
+
+Set up
+----------
+Copy default.install.sh to install.sh. Edit the file and adjust the variables.
+
+```bash
+  . ./install.sh
+```
+
+Good luck!
