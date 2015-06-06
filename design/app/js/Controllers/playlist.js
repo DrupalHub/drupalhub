95be89e24e44913c73c08906faf63146a29df478
Roy Segall <roy.segall@gizra.com>
2015-06-06 13:41:54 +0300

    fix the comments widget.

--- design/app/js/Controllers/playlist.js
+++ design/app/js/Controllers/playlist.js
@@ -1,12 +1,12 @@
 DrupalHub.controller('PlaylistCtrl', function($scope, DrupalHubRequest, $routeParams) {
 
-  $scope.playlist = {};
+  $scope.playlistObject = {};
   $scope.video = {};
 
   DrupalHubRequest.localRequest('get', 'playlist/' + $routeParams['id']).success(function(data) {
-    $scope.playlist = data.data[0];
+    $scope.playlistObject = data.data[0];
     $scope.delta = $routeParams['delta'] - 1;
-    $scope.video = $scope.playlist.videos[$routeParams['delta'] - 1];
-    $scope.youtube = '<iframe class="embed-responsive-item" src="https://www.youtube.com/embed/' + $scope.video.embed  + '?autoplay=true" frameborder="0" allowfullscreen></iframe>';
+    $scope.video = $scope.playlistObject.videos[$routeParams['delta'] - 1];
+    $scope.youtube = '<iframe class="embed-responsive-item" src="https://www.youtube.com/embed/' + $scope.video.embed  + '?autoplay=1" frameborder="0" allowfullscreen></iframe>';
   });
 });
--- design/app/js/Directives/comments/comments.js
+++ design/app/js/Directives/comments/comments.js
@@ -2,69 +2,68 @@ DrupalHub.directive('drupalHubComments', function($location, DrupalHubRequest) {
   return {
     restrict: 'AE',
     templateUrl: 'js/Directives/comments/element.html',
+    scope: {
+      nid: '=',
+      type: '@'
+    },
     link: function($scope) {
+      $scope.$watch('nid', function(nid) {
 
-      $scope.canComment = false;
-      DrupalHubRequest.userAccess('post comments').success(function(response) {
-        $scope.canComment = response.data.access;
-      });
+        if (nid == undefined) {
+          return;
+        }
 
-      // Display comment part.
-      var path = $location.path().split('/');
-      var nid = path[2];
-      var type = path[1];
+        var type = $scope.type;
 
-      if (type != 'playlist') {
-        DrupalHubRequest.localRequest('get', 'comments?nid=' + nid).success(function(data, status) {
+        $scope.canComment = false;
+        DrupalHubRequest.userAccess('post comments').success(function(response) {
+          $scope.canComment = response.data.access;
+        });
+
+        if (type == 'question') {
+          $scope.title = 'Help this question';
+          $scope.btnType = 'btn-primary';
+        }
+        else if (['video', 'playlist'].indexOf(type) >= 0) {
+          $scope.title = 'What do you think on this video?';
+          $scope.btnType = 'btn-danger';
+        }
+        else if (type == 'event') {
+          $scope.title = 'What do you think on this event?';
+          $scope.btnType = 'btn-success';
+        }
+        else {
+          $scope.title = 'What do you think on this blog?';
+          $scope.btnType = 'btn-warning';
+        }
+
+        DrupalHubRequest.localRequest('get', 'comments?nid=' + nid).success(function(data) {
           $scope.comments = data.data;
         });
-      }
 
-      if (type == 'question') {
-        $scope.title = 'Help this question';
-        $scope.btnType = 'btn-primary';
-      }
-      else if (['video', 'playlist'].indexOf(type) >= 0) {
-        $scope.title = 'What do you think on this video?';
-        $scope.btnType = 'btn-danger';
+        // Submit a comment part.
+        $scope.newComment = {
+          text: '',
+          nid: nid
+        };
 
-        if (type == 'playlist') {
-          DrupalHubRequest.localRequest('get', 'playlist/' + nid).success(function (data) {
+        $scope.submit = function() {
+          $scope.commentsError = '';
+          if ($scope.newComment.body == '') {
+            $scope.commentsError = "OOPS... It's look the comment is empty.";
+          }
 
-            DrupalHubRequest.localRequest('get', 'comments?nid=' + data.data[0].videos[path[3] - 1].id).success(function(data, status) {
-              $scope.comments = data.data;
-            });
-          });
-        }
-      }
-      else {
-        $scope.title = 'What do you think on this blog?';
-        $scope.btnType = 'btn-warning';
-      }
-
-      // Submit a comment part.
-      $scope.newComment = {
-        text: '',
-        nid: nid
-      };
-
-      $scope.submit = function() {
-      $scope.commentsError = '';
-        if ($scope.newComment.body == '') {
-          $scope.commentsError = "OOPS... It's look the comment is empty.";
-        }
-
-        if ($scope.commentForm.$valid) {
-          DrupalHubRequest.localRequest('post', 'comments', $scope.newComment).
-            success(function(data, status) {
+          if ($scope.commentForm.$valid) {
+            DrupalHubRequest.localRequest('post', 'comments', $scope.newComment).success(function(data) {
               $scope.comments.push(data.data[0]);
               $scope.newComment = {
                 text: '',
                 nid: nid
               };
             });
-        }
-      };
+          }
+        };
+      });
     }
   };
 });
