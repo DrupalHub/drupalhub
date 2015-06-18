ea97220b9bfee006f1fe312bc852031a29fd4cb8
nader77 <nader@gizra.com>
2015-06-18 20:19:37 +0300

    Add functionality to next and previous buttons in Playlist.

--- design/app/js/Controllers/playlist.js
+++ design/app/js/Controllers/playlist.js
@@ -1,12 +1,15 @@
-DrupalHub.controller('PlaylistCtrl', function($scope, DrupalHubRequest, $routeParams) {
+DrupalHub.controller('PlaylistCtrl', function($scope, DrupalHubRequest,  $routeParams) {
 
   $scope.playlistObject = {};
   $scope.video = {};
+  $scope.playlistId = $routeParams['id'];
 
-  DrupalHubRequest.localRequest('get', 'playlist/' + $routeParams['id']).success(function(data) {
+  DrupalHubRequest.localRequest('get', 'playlist/' + $scope.playlistId).success(function(data) {
     $scope.playlistObject = data.data[0];
     $scope.delta = $routeParams['delta'] - 1;
-    $scope.video = $scope.playlistObject.videos[$routeParams['delta'] - 1];
+    $scope.nextVideo = $scope.playlistObject.videos[$scope.delta + 1] ? $scope.delta + 2 : false;
+    $scope.previousVideo = $scope.playlistObject.videos[$scope.delta - 1] ? $scope.delta : false;
+    $scope.video = $scope.playlistObject.videos[$scope.delta];
     $scope.youtube = '<iframe class="embed-responsive-item" src="https://www.youtube.com/embed/' + $scope.video.embed  + '?autoplay=1" frameborder="0" allowfullscreen></iframe>';
   });
 });
