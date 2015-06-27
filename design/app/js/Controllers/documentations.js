5ab30171fc3a0fc88754d7e5a6203a7344af046d
Roy Segall <roy.segall@gizra.com>
2015-06-27 15:32:56 +0300

    #123: Documentation front page.

---
+++ design/app/js/Controllers/documentations.js
@@ -0,0 +1,15 @@
+DrupalHub.controller('documentationsCtrl', function($scope, DrupalHubRequest, $location) {
+  var page = $location.search().page == undefined ? 1 : $location.search().page;
+
+  DrupalHubRequest.localRequest('get', 'wiki?range=5&page=' + page).success(function(data) {
+    $scope.documentations = data.data;
+
+    DrupalHubRequest.userAccess('create wiki content').success(function(data) {
+      $scope.showCreate = data.data.access;
+    });
+  });
+
+  DrupalHubRequest.localRequest('get', 'wiki_category?range=5').success(function(data) {
+    $scope.documentation_tags = data.data;
+  });
+});
