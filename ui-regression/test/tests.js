91f316fb4a854c9261dc74fd3075e1bf448d0097
David kohav <david@gizra.com>
2015-06-27 09:52:00 +0300

    Update the ui-regression.

---
+++ ui-regression/test/tests.js
@@ -0,0 +1,59 @@
+'use strict';
+
+var shoovWebdrivercss = require('shoov-webdrivercss');
+
+// This is an example assuming BrowserStack is used, as the capabilities are
+// encoded in the way their system is using.
+// See https://www.browserstack.com/automate/node
+
+// This can be executed by passing the environment argument like this:
+// PROVIDER_PREFIX=browserstack SELECTED_CAPS=ie11 mocha
+// PROVIDER_PREFIX=browserstack SELECTED_CAPS=chrome mocha
+var capsConfig = {
+  'chrome': {
+    'browser' : 'Chrome',
+    'browser_version' : '39.0',
+    'os' : 'OS X',
+    'os_version' : 'Yosemite',
+    'resolution' : '1024x768'
+  },
+  'ie11': {
+    'browser' : 'IE',
+    'browser_version' : '11.0',
+    'os' : 'Windows',
+    'os_version' : '7',
+    'resolution' : '1024x768'
+  }
+}
+
+var selectedCaps = process.env.SELECTED_CAPS || undefined;
+var caps = selectedCaps ? capsConfig[selectedCaps] : undefined;
+
+var providerPrefix = process.env.PROVIDER_PREFIX ? process.env.PROVIDER_PREFIX + '-' : '';
+var testName = selectedCaps ? providerPrefix + selectedCaps : providerPrefix + 'default';
+
+var url ='http://33e177bf.ngrok.com';
+
+describe('Search engine tests', function() {
+
+  this.timeout(99999999);
+  var client = {};
+
+  before(function(done){
+    client = shoovWebdrivercss.before(done, caps);
+  });
+
+  after(function(done) {
+    shoovWebdrivercss.after(done);
+  });
+
+  it('should show the drupalhub home page',function(done) {
+    client
+      .url(url)
+      .webdrivercss(testName, {
+        name: 'hompage'
+      }, shoovWebdrivercss.processResults)
+      .call(done);
+
+  });
+});
