fb7c0970e8d7f682d076ebfae975e293ff428c2b
Roy Segall <roy.segall@gizra.com>
2015-06-27 16:20:50 +0300

    #126: Suggesting a documentation form.

--- drupalhub/modules/drupalhub/drupalhub_restful/plugins/restful/node/wiki/DrupalHubWiki.class.php
+++ drupalhub/modules/drupalhub/drupalhub_restful/plugins/restful/node/wiki/DrupalHubWiki.class.php
@@ -15,4 +15,36 @@ class DrupalHubWiki extends DrupalHubRestfulNode {
     return $public_fields;
   }
 
+  /**
+   * {@inheritdoc}
+   */
+  public function propertyValuesPreprocess($property_name, $value, $public_field_name) {
+    if ($public_field_name == 'tags') {
+      $query = new EntityFieldQuery();
+
+      $vocabulary = taxonomy_vocabulary_machine_name_load('wiki_category');
+
+      $results = $query
+        ->entityCondition('entity_type', 'taxonomy_term')
+        ->propertyCondition('name', $value)
+        ->propertyCondition('vid', $vocabulary->vid)
+        ->execute();
+
+      if (empty($results['taxonomy_term'])) {
+        // Create new term.
+        $term = new stdClass();
+        $term->name = $value;
+        $term->vid = $vocabulary->vid;
+        $term = entity_create('taxonomy_term', array('name' => $value, 'vid' => $vocabulary->vid));
+        entity_save('taxonomy_term', $term);
+      }
+      else {
+        $term = reset(array_keys($results['taxonomy_term']));
+      }
+
+      return $term->tid;
+    }
+    return parent::propertyValuesPreprocess($property_name, $value, $public_field_name);
+  }
+
 }
