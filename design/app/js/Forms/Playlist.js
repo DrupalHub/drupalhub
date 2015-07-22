b90d5cb3250e4cce79b41239d3d33b9589caf08f
Roy Segall <roy.segall@gizra.com>
2015-07-22 23:57:40 +0300

    more work.

--- design/app/js/Forms/Playlist.js
+++ design/app/js/Forms/Playlist.js
@@ -1,3 +1,22 @@
 DrupalHub.controller('PlayListFormCtrl', function($scope, DrupalHubRequest) {
 
+  $scope.results = '';
+  $scope.videos = [];
+
+  $scope.$watch('results', function(value) {
+    if (value == undefined) {
+      return;
+    }
+
+    if (value.originalObject == undefined) {
+      return;
+    }
+
+    $scope.videos.push(value.originalObject);
+  });
+
+  $scope.compilePlaylist = function() {
+    console.log($scope.videos);
+  };
+
 });
