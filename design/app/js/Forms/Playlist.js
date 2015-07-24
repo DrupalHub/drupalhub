f4ecbf016c59d9eb8a6367049644828a6a4bfe1f
Roy Segall <roy.segall@gizra.com>
2015-07-24 23:57:33 +0300

    Commit the work.

--- design/app/js/Forms/Playlist.js
+++ design/app/js/Forms/Playlist.js
@@ -2,26 +2,39 @@ DrupalHub.controller('PlayListFormCtrl', function($scope, DrupalHubRequest) {
 
   $scope.results = '';
   $scope.videos = [];
+  $scope.ids = [];
 
-  $scope.$watch('results', function(value) {
-    if (value == undefined) {
-      return;
-    }
-
-    if (value.originalObject == undefined) {
-      return;
-    }
-
-    $scope.videos.push(value.originalObject);
-  });
-
-  $scope.compilePlaylist = function() {
-    console.log($scope.videos);
+  $scope.updateVideos = function(value) {
+    $scope.videos.push(value);
+    $scope.ids.push(value.originalObject.id);
   };
 
-  $scope.alterRemoterResponse = function() {
-    console.log('foo');
-    debugger;
+  /**
+   * Alter the results for the auto complete widget. This how we prevent from
+   * selected videos to appear.
+   */
+  $scope.alterResults = function(value) {
+
+    if ($scope.ids.length == 0) {
+      return value;
+    }
+
+    var results = [];
+
+    for (var i = 0; i < value.data.length; i++) {
+      if ($scope.ids.indexOf(value.data[i].id) == -1) {
+        results.push(value.data[i]);
+      }
+    }
+
+    return {data: results};
   };
+
+  /**
+   * Delete the selected video from the list.
+   */
+  $scope.removeSelectedVideo = function(video) {
+
+  }
 
 });
