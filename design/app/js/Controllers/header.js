7023ca458e6f7d8d82db30e0cf1adf3ae2aa0970
Roy Segall <roy.segall@gizra.com>
2015-07-27 00:06:22 +0300

    Start working on the title of the page.

--- design/app/js/Controllers/header.js
+++ design/app/js/Controllers/header.js
@@ -1,5 +1,7 @@
 DrupalHub.controller('headerCtrl', function($scope, DrupalHubRequest) {
 
+  $scope.headerTitle = 'Drupal.org.il';
+
   // Alter the access token before the flag directive http request kicks in.
   $scope.$on('flagAccessToken', function(event, data) {
     data.accessToken = DrupalHubRequest.accessToken;
