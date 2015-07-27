362ba134d49726f13946f4ff334e100f942ad9ff
Roy Segall <roy.segall@gizra.com>
2015-07-27 23:35:02 +0300

    Fix the definition of the routing.

--- design/app/js/Controllers/header.js
+++ design/app/js/Controllers/header.js
@@ -2,6 +2,13 @@ DrupalHub.controller('headerCtrl', function($scope, DrupalHubRequest) {
 
   $scope.headerTitle = 'Drupal.org.il';
 
+  $scope.$on('$routeChangeStart', function(value, route) {
+
+    if (route.title) {
+      $scope.headerTitle = 'Drupal.org.il: ' + route.title;
+    }
+  });
+
   // Alter the access token before the flag directive http request kicks in.
   $scope.$on('flagAccessToken', function(event, data) {
     data.accessToken = DrupalHubRequest.accessToken;
