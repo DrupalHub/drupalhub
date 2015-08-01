d8d248cd646c84ce31586efad66873d15697a7f1
Roy Segall <roy.segall@gizra.com>
2015-08-02 00:00:37 +0300

    Delte question.

--- design/app/js/Controllers/question.js
+++ design/app/js/Controllers/question.js
@@ -1,7 +1,20 @@
-DrupalHub.controller('questionCtrl', function($scope, DrupalHubRequest, $routeParams, $rootScope) {
+DrupalHub.controller('questionCtrl', function($scope, DrupalHubRequest, $routeParams, $rootScope, dialogs) {
 
   DrupalHubRequest.localRequest('get', 'question/' + $routeParams['id'] + '?add_view=add').then(function(data) {
     $scope.question = data.data.data[0];
     $rootScope.$emit('titleAlter', $scope.question.label);
   });
+
+  /**
+   * Delete the question.
+   */
+  $scope.deleteQuestion = function() {
+    var dlg = dialogs.confirm('Delete the question', 'Are you sure you want to delete this question?');
+
+    dlg.result.then(function() {
+      DrupalHubRequest.localRequest('delete', 'question/' + $scope.question.id);
+      dialogs.notify('Deleted', 'The question has deleted.');
+      window.location = "#/";
+    });
+  };
 });
