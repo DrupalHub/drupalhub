397ca228170a98f314d24ee9b96d7b3abdf7ab25
Roy Segall <roy.segall@gizra.com>
2015-08-04 10:55:39 +0300

    For now - dev.

--- design/Gruntfile.js
+++ design/Gruntfile.js
@@ -370,7 +370,9 @@ module.exports = function (grunt) {
         },
         constants: {
           Config: {
-            'backend': 'http://dev-drupalhub.gotpantheon.com/api/'
+            'backend': 'http://dev-drupalhub.gotpantheon.com/api/',
+            'pusher_key': '46b578a29d7e3df90f07',
+            'pusher_channel': 'drupalhub_drupalhub'
           }
         }
       }
