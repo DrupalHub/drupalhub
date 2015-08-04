fe2563d1773ab76714d909513187061e3b8f483f
Roy Segall <roy.segall@gizra.com>
2015-08-04 10:56:07 +0300

    Built DrupalHub from commit 397ca22 on branch master

--- js/config/config.js
+++ js/config/config.js
@@ -1,5 +1,5 @@
 angular.module('DrupalHubConfig', [])
 
-.constant('Config', {backend:'http://dev-drupalhub.gotpantheon.com/api/'})
+.constant('Config', {backend:'http://dev-drupalhub.gotpantheon.com/api/',pusher_key:'46b578a29d7e3df90f07',pusher_channel:'drupalhub_drupalhub'})
 
 ;
