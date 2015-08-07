e31000303b0a8260f6c77522bc17d5ef65179e7a
Roy Segall <roy.segall@gizra.com>
2015-08-07 23:52:17 +0300

    Start work on the repsonsive.

--- design/Gruntfile.js
+++ design/Gruntfile.js
@@ -59,7 +59,7 @@ module.exports = function (grunt) {
         port: 9000,
         livereload: 35729,
         // change this to '0.0.0.0' to access the server from outside
-        hostname: '127.0.0.1'
+        hostname: '0.0.0.0'
       },
       livereload: {
         options: {
