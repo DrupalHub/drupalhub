d1b9301bd70d75679c7b02f7cf9d8fbf89c91e4c
Roy Segall <roy.segall@gizra.com>
2015-08-12 00:07:18 +0300

    Minor fix to the isolated scope.

--- design/app/js/Directives/up_down/DrupalHubPager.js
+++ design/app/js/Directives/up_down/DrupalHubPager.js
@@ -2,9 +2,21 @@ DrupalHub.directive('drupalhubUpDown', function(DrupalHubRequest, $location) {
   return {
     restrict: 'AE',
     scope: {
+      id: '@',
+      type: '@'
     },
     templateUrl: 'js/Directives/up_down/element.html',
     link: function($scope) {
+      $scope.$watchGroup(['id', 'type'], function(value) {
+        var id = +value[0];
+
+        if (!id) {
+          return;
+        }
+        DrupalHubRequest.localRequest('get', 'voting_api?filter[entity_type]=' + value[1] + '&filter[entity_id]=' + id).then(function(data) {
+          console.log(data);
+        });
+      });
     }
   };
 });
