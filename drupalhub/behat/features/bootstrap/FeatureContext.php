913ba25a13af650cad405bf211c79dd8b4ba2061
Roy Segall <roy.segall@gizra.com>
2015-08-23 23:11:19 +0300

    Add screenshot for failure.

--- drupalhub/behat/features/bootstrap/FeatureContext.php
+++ drupalhub/behat/features/bootstrap/FeatureContext.php
@@ -1,15 +1,55 @@
 <?php
 
-use Behat\Behat\Context\ClosuredContextInterface,
-    Behat\Behat\Context\TranslatedContextInterface,
-    Behat\Behat\Context\BehatContext,
-    Behat\Behat\Exception\PendingException;
-use Behat\Gherkin\Node\PyStringNode,
-    Behat\Gherkin\Node\TableNode;
+use Behat\Gherkin\Node\TableNode;
 use Drupal\DrupalExtension\Context\DrupalContext;
 
 /**
  * Features context.
  */
 class FeatureContext extends DrupalContext {
+
+  /**
+   * @var String
+   *
+   * The screen shot path.
+   */
+  public $screenShotPath;
+
+  /**
+   * Overrides the class save screen shoot method.
+   */
+  public function saveScreenShot($filename = null, $filepath = null) {
+    $driver = $this->getSession()->getDriver();
+    $screen_shot = $driver->getScreenshot();
+    $this->screenShotPath = getcwd() . '/screenshots/' . time() . '.png';
+
+    file_put_contents($this->screenShotPath, $screen_shot);
+  }
+
+  /**
+   * Throw exception with picture.
+   *
+   * @param $text
+   *   The text. The screen shot image will be added.
+   *
+   * @throws Exception
+   */
+  protected function throwException($text) {
+    $this->saveScreenshot();
+    $text .= sprintf('Look on the screen shot at %s', $this->screenShotPath);
+    throw new \Exception($text);
+  }
+
+  /**
+   * @Then /^I should see the links:$/
+   */
+  public function iShouldSeeTheLinks(TableNode $table) {
+    foreach ($table->getRows() as $row) {
+      try {
+        $this->assertSession()->pageTextContains($this->fixStepArgument($row[0]));
+      } catch (\Exception $e) {
+        $this->throwException(sprintf('The tests did not find the text %s', $row[0]));
+      }
+    }
+  }
 }
--- drupalhub/behat/features/sanity-check.feature
+++ drupalhub/behat/features/sanity-check.feature
@@ -3,4 +3,15 @@ Feature: Sanity check
   @javascript
   Scenario: Check front page
     Given I visit "/"
-    Then I should see "Questions"
+    Given I should see "a"
+    Then I should see the links:
+      | DrupalHub     |
+      | Events        |
+      | Videos        |
+      | Documentation |
+      | Search        |
+      | Social Up!    |
+      | Login/Sign in |
+      | Events        |
+      | Last videos   |
+      | Blogs         |
