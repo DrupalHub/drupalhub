c50440eb09284a8de6f1203da4c8007476ee10d2
Martijn de Geus <martijn@side-shore.nl>
2015-09-11 13:35:22 +0200

    Revert "Local setings"
    This reverts commit 6be385de58be8022f7fa6675beef469bccf4ed61.

--- design/app/_settings/live.js
+++ design/app/_settings/live.js
@@ -1,4 +1,4 @@
 // Implements for flag directive the address for endpoint.
 angular.module('flagConfig', []).constant('flagConfig', {
-  'server': 'http://api.adrenalinehub.dev/api/'
+  'server': 'http://dev-drupalhub.gotpantheon.com/api/'
 });
