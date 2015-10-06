6aa04ca9549ae8624cbf470dd44b7221c219f57a
Roy Segall <roy.segall@gizra.com>
2015-10-06 22:54:55 +0300

    More work on the translation.

--- design/app/js/Controllers/event.js
+++ design/app/js/Controllers/event.js
@@ -1,4 +1,4 @@
-DrupalHub.controller('eventCtrl', function($scope, DrupalHubRequest, $routeParams, $rootScope) {
+DrupalHub.controller('eventCtrl', function($scope, DrupalHubRequest, $routeParams, $rootScope, $filter) {
 
   DrupalHubRequest.localRequest('get', 'event/' + $routeParams.id)
     .then(function(data) {
@@ -6,15 +6,22 @@ DrupalHub.controller('eventCtrl', function($scope, DrupalHubRequest, $routeParam
       $rootScope.$emit('titleAlter', $scope.event.label);
     });
 
-  $scope.rsvpStatus = 'Unknown';
+  $scope.rsvpStatus = $filter('translate')('Unknown');
   $scope.rsvpID = 0;
+
+  $scope.tablLalbes = {
+    'Status:': $filter('translate')('Status'),
+    'Not': $filter('translate')('Not'),
+    'Maybe': $filter('translate')('Maybe'),
+    'Coming': $filter('translate')('Coming')
+  };
 
   DrupalHubRequest.localRequest('get', 'rsvp').then(function (data) {
     if (data.data.count != 0) {
       var rsvp = data.data.data[0];
 
       $scope.rsvpID = rsvp.id;
-      $scope.rsvpStatus = rsvp.rsvp_status;
+      $scope.rsvpStatus = $filter('translate')(rsvp.rsvp_status);
     }
   });
 
@@ -36,7 +43,7 @@ DrupalHub.controller('eventCtrl', function($scope, DrupalHubRequest, $routeParam
       'rsvp_status': status,
       'event': $scope.event.id
     }).then(function (data) {
-      $scope.rsvpStatus = status;
+      $scope.rsvpStatus = $filter('translate')(status);
 
       DrupalHubRequest.localRequest('get', 'event/' + $routeParams.id).then(function(data) {
         var new_event = data.data.data[0];
