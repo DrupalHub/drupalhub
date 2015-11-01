909f1c0435180860f5fc477a4d4d2a4d0a3e0443
RoySegall <roy.segall@gizra.com>
2015-11-01 16:11:51 +0200

    Built DrupalHub from commit 160f402 on branch master

--- js/config/config.js
+++ js/config/config.js
@@ -1,5 +1,5 @@
 angular.module('DrupalHubConfig', [])
 
-.constant('Config', {backend:'http://live-drupalhub.gotpantheon.com/api/',front:'http://www.drupal.org.il',pusher_key:'1de96d1569d9d0cbc313',pusher_channel:'drupalhub_drupalhub'})
+.constant('Config', {backend:'http://live-drupalhub.gotpantheon.com/api/',front:'http://hub.drupal.org.il',pusher_key:'1de96d1569d9d0cbc313',pusher_channel:'drupalhub_drupalhub'})
 
 ;
