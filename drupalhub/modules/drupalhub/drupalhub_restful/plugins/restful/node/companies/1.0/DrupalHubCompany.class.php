8930e321c8e589b9fbfca3036ca79bd88c2d9d0b
Roy Segall <roy.segall@gizra.com>
2016-03-25 00:28:50 +0200

    Done working on the end point.

--- drupalhub/modules/drupalhub/drupalhub_restful/plugins/restful/node/companies/1.0/DrupalHubCompany.class.php
+++ drupalhub/modules/drupalhub/drupalhub_restful/plugins/restful/node/companies/1.0/DrupalHubCompany.class.php
@@ -8,7 +8,57 @@ class DrupalHubCompany extends \DrupalHubRestfulNode {
   public function publicFieldsInfo() {
     $public_fields = parent::publicFieldsInfo();
 
+    $public_fields['telephone'] = array(
+      'property' => 'field_telephone',
+    );
+
+    $public_fields['technologies'] = array(
+      'property' => 'field_technologies',
+      'resource' => array(
+        'technlogies' => 'technologies',
+      ),
+    );
+
+    $public_fields['site_address'] = array(
+      'property' => 'field_site_address',
+      'sub_property' => 'url',
+    );
+
+    $public_fields['email'] = array(
+      'property' => 'field_email',
+    );
+
+    $public_fields['hiring'] = array(
+      'property' => 'field_hiring',
+    );
+
+    $public_fields['hiring_mail'] = array(
+      'property' => 'field_email_hiring_field',
+    );
+
+    $public_fields['logo'] = array(
+      'property' => 'field_logo',
+    );
+
+    $public_fields['addresses'] = array(
+      'property' => 'field_addresses',
+      'process_callbacks' => array(
+        array($this, 'processAddresses'),
+      ),
+    );
+
     return $public_fields;
+  }
+
+  protected function processAddresses($value) {
+    array_walk($value, function(&$value) {
+      $value = array(
+        'lat' => $value['lat'],
+        'lng' => $value['lat'],
+      );
+    });
+
+    return $value;
   }
 
 }
---
+++ drupalhub/modules/drupalhub/drupalhub_restful/plugins/restful/taxonomy_term/technologies/1.0/DrupalHubTechnologies.class.php
@@ -0,0 +1,28 @@
+<?php
+
+class DrupalHubTechnologies extends RestfulEntityBaseTaxonomyTerm {
+
+  /**
+   * {@inheritdoc}
+   */
+  public function publicFieldsInfo() {
+    $public_fields = parent::publicFieldsInfo();
+
+    $public_fields['url'] = array(
+      'property' => 'tid',
+      'process_callbacks' => array(
+        array($this, 'processUrl'),
+      ),
+    );
+
+    return $public_fields;
+  }
+
+  /**
+   * Render the item.
+   */
+  public function processUrl($tid) {
+    return 'tags.html/#' . $tid;
+  }
+
+}
