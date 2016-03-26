a8d2036816915d1c4b731fe202507db774bdf18a
Roy Segall <roy.segall@gizra.com>
2016-03-26 13:26:45 +0300

    Done with the basic company memberships

--- drupalhub/modules/drupalhub/drupalhub_restful/plugins/restful/og_membership/company_membership/1.0/DrupalHubCompanyMembership.class.php
+++ drupalhub/modules/drupalhub/drupalhub_restful/plugins/restful/og_membership/company_membership/1.0/DrupalHubCompanyMembership.class.php
@@ -2,4 +2,27 @@
 
 class DrupalHubCompanyMembership extends \RestfulEntityBase {
 
+  /**
+   * {@inheritdoc}
+   */
+  public function publicFieldsInfo() {
+    $fields = parent::publicFieldsInfo();
+
+    $fields['role'] = array(
+      'property' => 'field_role_title',
+    );
+
+    $fields['group'] = array(
+      'property' => 'group',
+      'sub_property' => 'nid',
+    );
+
+    $fields['uid'] = array(
+      'property' => 'entity',
+      'sub_property' => 'uid',
+    );
+
+    return $fields;
+  }
+
 }
